8ceda05f7dfbb7846458c05dad25b7197d18e9d9
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-15 19:23:42 +0530

    CSS: Cursor Properties

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
 console.clear();
 // Functions
-
-// function expression 
-function calculateTip(price, percentage) {
-    // side effect
-    console.log('Calculate Tip');
-    return (percentage / 100) * price
-}
-let calculateTipResult = calculateTip(100, 5);
-
-console.log(calculateTipResult); // 5
-
-// function declaration
-
-let divideByN = function (number, n) {
-    return number / n;
-}
-let divideByNResult = divideByN(20, 4)
-console.log(divideByNResult);// 5
-
-// anonymous function 
