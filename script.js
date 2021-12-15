757a464b25800ea4987b4fc0096b09e5be0bf006
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-15 20:47:53 +0530

    JavaScript Functions

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 console.clear();
 // JavaScript Cookies
 
-function writeCookie() {
-    if (document.myform.customer.value == '') {
-        alert('Enter Same Value');
-        return
-    }
-    let cookievalue = escape(document.myform.customer.value) + ';';
-    document.cookie = 'name' + cookievalue;
-    console.log('Setting Cookie: ' + 'Name = ' + cookievalue);
+function sayHello(name, age) {
+    alert(name + ' is ' + age + ' years old!')
 }
