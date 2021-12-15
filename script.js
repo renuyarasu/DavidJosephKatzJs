6d2034ec2a2138ff6d91ebb47be574b2e835765a
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-15 19:35:00 +0530

    Do while loop

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.clear();
 // Functions
+
+let actualPin = '1115';
+let pin;
+let count = 0;
+
+do {
+    pin = prompt("Enter your pin...");
+    count++;
+
+} while (pin != actualPin && count < 3);
+if (pin == actualPin) {
+    console.log('Welcome User!');
+} else {
+    console.log('Account Locaked!');
+}
