3836c80feed20e67db6b3735501592df8c2aef78
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-22 20:22:55 +0530

    JavaScript: Loop Control Statements - Continue

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.clear();
 // JavaScript:  Loop Control Statements [Break, Continue]
 
-
+/* Continue Statement
+It forces to the next iteration of the loop to take place, skipping any code in between*/
 for (let i = 0; i < 10; i++) {
     if (i == 5)
-    break;
+    continue;
         document.write(i + '<br>')
 }
