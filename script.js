464d658586650cdcac7ad193edc88a7c825795d8
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-22 20:29:46 +0530

    JavaScript: Logical Operators - .

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 console.clear();
-// JavaScript:  Loop Control Statements [Break, Continue]
+// JavaScript:   Logical Operators (&&, ||, !)
+let userId = prompt('Enter user Id', 'User Id')
+let pwd = prompt('Enter your password', 'Password')
 
-/* Continue Statement
-It forces to the next iteration of the loop to take place, skipping any code in between*/
-for (let i = 0; i < 10; i++) {
-    if (i == 5)
-    continue;
-        document.write(i + '<br>')
-}
+let message = (userId == 'VedaGna' && pwd == '1115') ? 'Welcome' : 'Invalid User';
+alert(message)
