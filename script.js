a3d254aec4c225a06e45b3f83621c966aeb6ecf6
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-22 22:46:50 +0530

    JavaScript: Animation

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 console.clear();
-// JavaScript: If Else Statement
+// JavaScript: Animation
 
-let marks_01 = parseInt(prompt('Enter marks 01', 'Enter...'))
-let marks_02 = parseInt(prompt('Enter marks 02', 'Enter...'))
-let marks_03 = parseInt(prompt('Enter marks 03', 'Enter...'))
+let newArray = ['puppy_bw.png', 'puppy_c.png'];
+let i = 1;
+let ref;
 
-let total = marks_01 + marks_02 + marks_03;
-let percentage = total * 100 / 150;
-console.log(percentage);
-
-if (percentage >= 80) {
-    console.log('Grade A');
-} else if (percentage >= 60) {
-    console.log('Grade B');
-} else if (personalbar >= 40) {
-    console.log('Grade C');
-} else {
-    console.log('Grade D');
+function galery() {
+    document.images[0].src = newArray[i];
+    i++;
+    if (i < 3)
+        i = 0;
+}
+function start(){
+    ref= setInterval('galery()', 1000)
+};
+function stop(){
+    clearInterval(ref)
 }
