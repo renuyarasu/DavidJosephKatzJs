7e199a180274ac3838385220ed57cab20440e564
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-22 21:41:34 +0530

    JavaScript: Switch Statement - Shorthand

--- script.js
+++ script.js
@@ -4,19 +4,11 @@ console.clear();
 let vowels = prompt('Enter  a character', 'Enter...');
 switch (vowels) {
     case 'a':
-        console.log('A is vowel!');
-        break;
     case 'e':
-        console.log('E is vowel!');
-        break;
     case 'i':
-        console.log('I is vowel!');
-        break;
     case 'o':
-        console.log('O is vowel!');
-        break;
     case 'u':
-        console.log('U is vowel!');
+        console.log(vowels + ' is vowel!');
         break;
     default:
         console.log('Consonant!');
